12ad9e224fcb89166c0093397f7ea024c275473c
Wilhelm-Zimmermann <wilhelmzimmermann8@gmail.com>
2022-12-22 16:07:42 -0300

    Product

--- src/main/java/com/devsuperior/course_java/config/TestConfig.java
+++ src/main/java/com/devsuperior/course_java/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.devsuperior.course_java.entities.Category;
 import com.devsuperior.course_java.entities.Order;
+import com.devsuperior.course_java.entities.Product;
 import com.devsuperior.course_java.entities.User;
 import com.devsuperior.course_java.entities.enums.OrderStatus;
 import com.devsuperior.course_java.repositories.CategoryRepository;
 import com.devsuperior.course_java.repositories.OrderRepository;
+import com.devsuperior.course_java.repositories.ProductRepository;
 import com.devsuperior.course_java.repositories.UserRepository;
 
 @Configuration
@@ -21,24 +23,33 @@ import com.devsuperior.course_java.repositories.UserRepository;
 public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
-	@Autowired 
+	@Autowired
 	private OrderRepository orderRepository;
 	@Autowired
 	private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
-		
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.WAITING_PAYMENT);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.PAID);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.PAID);
-		
+
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
 		Category c1 = new Category(null, "Technology");
-		
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(c1));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	}
 }
