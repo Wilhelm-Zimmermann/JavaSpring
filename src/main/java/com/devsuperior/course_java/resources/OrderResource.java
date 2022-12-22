43864825aa331c2f9769bbcc800383209fd2c489
Wilhelm-Zimmermann <wilhelmzimmermann8@gmail.com>
2022-12-22 14:10:08 -0300

    Order, Instant, ISO 8601 Completed

---
+++ src/main/java/com/devsuperior/course_java/resources/OrderResource.java
@@ -0,0 +1,32 @@
+package com.devsuperior.course_java.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.course_java.entities.Order;
+import com.devsuperior.course_java.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+	@Autowired
+	private OrderService orderService;
+
+	@GetMapping
+	public ResponseEntity<List<Order>> findAll() {
+		var orders = orderService.findAll();
+		return ResponseEntity.ok().body(orders);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
+		var order = orderService.findById(id);
+		return ResponseEntity.ok().body(order);
+	}
+}
